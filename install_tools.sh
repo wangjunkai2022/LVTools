c10df55e27768747cd9b91cb08add7693f572077
xiaokai <xiaokai@kk.com>
2023-07-07 16:41:05 +0700

    修改 权限和存储位置

--- install_tools.sh
+++ install_tools.sh
@@ -19,49 +19,51 @@ function docker_install() {
 }
 docker_install
 
+uid=1000
+#gid=1000
 echo "开始安装Video需要的docker插件"
 
-docker pull linuxserver/prowlarr
-docker pull linuxserver/sonarr
-docker pull linuxserver/radarr
+docker pull linuxserver/prowlarr:latest
+docker pull linuxserver/sonarr:latest
+docker pull linuxserver/radarr:latest
 docker pull p3terx/aria2-pro
-docker pull emby/embyserver
+docker pull emby/embyserver:latest
 docker pull portainer/portainer-ce
-docker pull linuxserver/ombi
-docker pull allanpk716/chinesesubfinder
-docker pull linuxserver/bazarr
+docker pull linuxserver/ombi:latest
+docker pull allanpk716/chinesesubfinder:latest
+docker pull linuxserver/bazarr:latest
 docker pull xhofe/alist
 
 docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce
 
-mkdir -p ~/videos/data/电影
-mkdir -p ~/videos/data/电视
-mkdir -p ~/videos/data/下载
+mkdir -p ~/videos/media/movie
+mkdir -p ~/videos/media/series
+mkdir -p ~/videos/downloads
 
 mkdir -p ~/videos/tools/prowlarr
 docker run -d --name=prowlarr -p 9696:9696 -v ~/videos/tools/prowlarr:/config --restart always linuxserver/prowlarr
 
 mkdir -p ~/videos/tools/sonarr
-docker run -d --name=sonarr -p 8989:8989 -v ~/videos/tools/sonarr:/config -v ~/videos/data/电视:/media/电视 -v ~/videos/data/下载:/mnt/下载 --restart always linuxserver/sonarr
+docker run -d --name=sonarr -e uid=$uid -p 8989:8989 -v ~/videos/tools/sonarr:/config -v ~/videos/media:/media -v ~/videos/downloads:/mnt/downloads --restart always linuxserver/sonarr
 
 mkdir -p ~/videos/tools/radarr
-docker run -d --name=radarr -p 7878:7878 -v ~/videos/tools/radarr:/config -v ~/videos/data/电影:/media/电影 -v ~/videos/data/下载:/mnt/下载 --restart always linuxserver/radarr
+docker run -d --name=radarr -e uid=$uid -p 7878:7878 -v ~/videos/tools/radarr:/config -v ~/videos/media:/media -v ~/videos/downloads:/mnt/downloads --restart always linuxserver/radarr
 
 mkdir -p ~/videos/tools/aria2
-docker run -d --name aria2-pro --restart always --log-opt max-size=1m -e RPC_PORT=6800 -p 6800:6800 -p 6888:6888 -p 6888:6888/udp -v ~/videos/tools/aria2:/config -v ~/videos/data/下载:/downloads p3terx/aria2-pro
+docker run -d --name aria2-pro -e uid=$uid --restart always --log-opt max-size=1m -e RPC_PORT=6800 -p 6800:6800 -p 6888:6888 -p 6888:6888/udp -v ~/videos/tools/aria2:/config -v ~/videos/downloads:/downloads p3terx/aria2-pro
 
 mkdir -p ~/videos/tools/embyserver
-docker run -d --name embyserver -v ~/videos/tools/embyserver:/config -v ~/videos/data/电视:/media/电视 -v ~/videos/data/电影:/media/电影 -p 8096:8096 -p 8920:8920 --restart always emby/embyserver
+docker run -d --name embyserver -e uid=$uid -v ~/videos/tools/embyserver:/config -v ~/videos/media:/media -p 8096:8096 -p 8920:8920 --restart always emby/embyserver
 
 mkdir -p ~/videos/tools/ombi
 docker run -d --name=ombi -e BASE_URL=/ombi -p 3579:3579 -v ~/videos/tools/ombi:/config --restart always linuxserver/ombi
 
 mkdir -p ~/videos/tools/chinesesubfinder/config
 mkdir -p ~/videos/tools/chinesesubfinder/browser
-docker run -d --name chinesesubfinder -v ~/videos/tools/chinesesubfinder/config:/config -v ~/videos/data/电影:/media/电影 -v ~/videos/data/电视:/media/电视 -v ~/videos/tools/chinesesubfinder/browser:/root/.cache/rod/browser -p 19035:19035 -p 19037:19037 --log-driver "json-file" --log-opt "max-size=10m" --restart=always allanpk716/chinesesubfinder
+docker run -d --name chinesesubfinder -v -e uid=$uid ~/videos/tools/chinesesubfinder/config:/config -v ~/videos/media:/media -v ~/videos/tools/chinesesubfinder/browser:/root/.cache/rod/browser -p 19035:19035 -p 19037:19037 --log-driver "json-file" --log-opt "max-size=10m" --restart=always allanpk716/chinesesubfinder
 
 mkdir -p ~/videos/tools/bazarr
-docker run -d --name=bazarr -p 6767:6767 -v ~/videos/tools/bazarr:/config -v ~/videos/data/电影:/media/电影 -v ~/videos/data/电视:/media/电视 --restart always linuxserver/bazarr
+docker run -d --name=bazarr -e uid=$uid -p 6767:6767 -v ~/videos/tools/bazarr:/config -v ~/videos/media:/media --restart always linuxserver/bazarr
 
 mkdir -p ~/videos/tools/alist
 docker run -d --restart=always -v ~/videos/tools/alist:/opt/alist/data -p 5244:5244 --name="alist" xhofe/alist
