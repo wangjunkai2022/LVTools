be57da1ef07fc8a05972105ce346771a30ccda2b
xiaokai <xiaokai@kk.com>
2023-07-07 20:30:43 +0700

    取消注释

--- install_ping.sh
+++ install_ping.sh
@@ -13,8 +13,8 @@ if [ "$answer" == "y" ]; then
 
   sed "s/0.0.0.0/$input_ip/g" -i ./auto_ping.sh
 
-#  sudo mv ./auto_ping.sh /bin/yy_auto_ping.sh
-#  sudo chmod 777 /bin/yy_auto_ping.sh
-#  sudo crontab -l >conf && echo "*/1 * * * * yy_auto_ping.sh" >>conf && sudo crontab conf && sudo rm -f conf
-#  sudo rm -rf ./install_ping.sh
+  sudo mv ./auto_ping.sh /bin/yy_auto_ping.sh
+  sudo chmod 777 /bin/yy_auto_ping.sh
+  sudo crontab -l >conf && echo "*/1 * * * * yy_auto_ping.sh" >>conf && sudo crontab conf && sudo rm -f conf
+  sudo rm -rf ./install_ping.sh
 fi
