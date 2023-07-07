c7b6df100af9a4bf68e6fea89063d9764cdfea9e
xiaokai <xiaokai@kk.com>
2023-07-07 16:39:53 +0700

    定时ping和关机

--- install_ping.sh
+++ install_ping.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 # 安装定时ping路由 需要先去设置路由ip的地址
-sudo mv ./auto_ping.sh /bin/yy_auto_ping.sh
-sudo chmod 777 /bin/yy_auto_ping.sh
-sudo crontab -l >conf && echo "*/1 * * * * yy_auto_ping.sh" >>conf && sudo crontab conf && sudo rm -f conf
-sudo rm -rf ./install_ping.sh
+
+function echo_green() {
+  echo -e "\033[32m$1\033[0m"
+}
+echo_green "是否执行ping不通自动关机的脚本添加到系统定时执行?(y/n)"
+read answer
+if [ "$answer" == "y" ]; then
+  cd $(dirname $0)
+  echo_green "请输入需要ping的ip地址 一般填写路由器的ip即可"
+  read input_ip
+
+  sed "s/0.0.0.0/$input_ip/g" -i ./auto_ping.sh
+
+#  sudo mv ./auto_ping.sh /bin/yy_auto_ping.sh
+#  sudo chmod 777 /bin/yy_auto_ping.sh
+#  sudo crontab -l >conf && echo "*/1 * * * * yy_auto_ping.sh" >>conf && sudo crontab conf && sudo rm -f conf
+#  sudo rm -rf ./install_ping.sh
+fi
