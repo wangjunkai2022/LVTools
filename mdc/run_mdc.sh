d40450c507a83f68e8d661d052ab0e59fa32b4ae
xiaokai <xiaokai@kk.com>
2023-07-21 20:35:12 +0700

    更改命令

--- mdc/run_mdc.sh
+++ mdc/run_mdc.sh
@@ -6,5 +6,4 @@ mkdir test
 dd if=/dev/zero of="./test/MIFD-046.mp4" bs=250MB count=1
 #docker run --rm --name mdc_test -it -v ${PWD}/test:/data -v ${PWD}/config:/config -e UID=$(stat -c %u test) -e GID=$(stat -c %g test) ghcr.io/vergilgao/mdc:latest
 #github AVDC
-
-docker run --rm --name mdc_test -it -v ${PWD}/test:/data -v ${PWD}/config:/config -v ${PWD}/videos/media/movie/JAV:/JAV_output -e UID=$(stat -c %u test) -e GID=$(stat -c %g test) ghcr.io/vergilgao/mdc:latest
+docker run --rm --name mdc_test -it -v ${PWD}/videos:/videos -v ${PWD}/config:/config -e UID=$(stat -c %u test) -e GID=$(stat -c %g test) ghcr.io/vergilgao/mdc:latest
