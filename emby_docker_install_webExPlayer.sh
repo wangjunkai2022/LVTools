32fec23d2c5d7c599e03ad81559936e50e1bbffe
xiaokai <xiaokai@kk.com>
2023-08-09 10:44:55 +0700

    emby web 外部播放更新代理

--- emby_docker_install_webExPlayer.sh
+++ emby_docker_install_webExPlayer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # 这个文件是复制到embyserver 的docker容器中运行的文件 在容器中执行添加web外部播放内容
 if [ -f /system/dashboard-ui/embyWebAddExternalUrl/embyLaunchPotplayer.js ]; then
-  echo "文件存在"
+  echo "此emby server 已经安装了web外部播放"
 else
-  echo "文件不存在"
+  echo "此emby server 没有安装web外部播放"
   wget https://github.com/bpking1/embyExternalUrl/archive/refs/tags/v0.0.6.zip
   unzip v0.0.6.zip
   cp -r embyExternalUrl-0.0.6/embyWebAddExternalUrl/ /system/dashboard-ui/
@@ -11,4 +11,4 @@ else
   rm v0.0.6.zip
   rm -rf embyExternalUrl-0.0.6
 fi
-rm emby_docker_install_webExPlayer.sh
+rm $0
--- install_emby_othe_play.sh
+++ install_emby_othe_play.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # 安装 embyserver web 第三方应用打开
 cd $(dirname $0)
-docker cp emby_docker_install_webExPlayer.sh embyserver:/
-docker exec embyserver sh emby_docker_install_webExPlayer.sh
+echo -e 'git 在大陆无法连接网络 请输入http代理 \n不需要输入"http://"直接输入ip:p和端口即可。。\n如: "192.168.50.2:10809" \n如果不用则直接输入回车'
+read proxy_ip
+if [ -z "$proxy_ip" ]; then
+  echo "不走代理下载"
+  sed "2i\echo '不走代理下载'" emby_docker_install_webExPlayer.sh >emby_docker_install_webExPlayer_temp.sh
+#  sed '7i\  wget https://github.com/bpking1/embyExternalUrl/archive/refs/tags/v0.0.6.zip' emby_docker_install_webExPlayer.sh >emby_docker_install_webExPlayer_temp.sh
+else
+  echo "代理为:$proxy_ip"
+  sed "2i\export http_proxy=http://$proxy_ip \nexport https_proxy=http://$proxy_ip" emby_docker_install_webExPlayer.sh >emby_docker_install_webExPlayer_temp.sh
+#  sed '7i\  wget -e "https_proxy=http://$proxy_ip" https://github.com/bpking1/embyExternalUrl/archive/refs/tags/v0.0.6.zip' emby_docker_install_webExPlayer.sh >emby_docker_install_webExPlayer_temp.sh
+fi
+docker cp emby_docker_install_webExPlayer_temp.sh embyserver:/
+docker exec embyserver sh emby_docker_install_webExPlayer_temp.sh
+rm emby_docker_install_webExPlayer_temp.sh
 docker restart embyserver
