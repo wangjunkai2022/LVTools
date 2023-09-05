a47cfee4870f5f07d99c9a51aca20f1621f4b8a7
xiaokai <xiaokai@kk.com>
2023-09-05 16:44:18 +0700

    rclone 创建命令 emby 播放源地址的 alist 的网盘 配合rclone使用

---
+++ rclone-info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker volume create rclonevolume -d rclone -o type=sftp -o sftp-host=_hostname_ -o sftp-user=_username_ -o sftp-pass=_password_ -o allow-other=true
+#运行前需要先 创建一个 名字为 alist_webdav 的webdav 服务
+#创建rclone的配置文件
+docker run -it --rm --name rclone_temp -v /data/videos/tools/rclone/config:/config/rclone rclone/rclone config
+#查看是否生成了 alist_webdav 的配置
+docker run -it --rm --name rclone_temp -v /data/videos/tools/rclone/config:/config/rclone rclone/rclone listremotes
+#把 alist_webdav 挂载到硬盘 /data/videos/media/alist 中
+docker run -d --name rclone-alist -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v /data/videos/tools/rclone/config:/config/rclone -v /data/videos/media/alist:/data:shared -v /data/videos/tools/rclone/cache:/cache --device /dev/fuse:/dev/fuse --cap-add SYS_ADMIN --security-opt apparmor:unconfined --restart always rclone/rclone mount alist_webdav:/ /data --cache-dir /cache --allow-other --vfs-cache-mode writes --allow-non-empty
