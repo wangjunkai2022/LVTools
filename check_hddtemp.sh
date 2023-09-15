d5793356bab5132412195d4762092218d8e03b51
xiaokai <xiaokai@kk.com>
2023-09-15 15:49:16 +0700

    debug ssh4

--- check_hddtemp.sh
+++ check_hddtemp.sh
@@ -4,18 +4,13 @@ hdds=(/dev/sdb /dev/sdc)
 
 function check() {
   # 检测硬盘温度
-  echo "$1"
   temp=$(hddtemp $1)
-  echo
-  echo "$temp"
-
   # 匹配温度值
   temp_regex='[+-]?[0-9]+([.][0-9]+)?°?[CF]'
   temp=$(echo "$temp" | grep -oE "$temp_regex")
 
   # 温度值
-  echo "$temp"
-  
+  temp=${temp:0:2}
   ## 值是否大于50度
   if [ $temp -gt 50 ]; then
     echo "$1 当前硬盘温度是 $temp 大于设置的温度50 现在执行关机."
