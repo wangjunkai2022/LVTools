b3355755708d6d6cd9c00cb9f86a7c403d7ac2d3
xiaokai <xiaokai@kk.com>
2023-09-15 15:32:58 +0700

    取消字符串

--- check_hddtemp.sh
+++ check_hddtemp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hdds=("/dev/sdb","/dev/sdc")
+hdds=(/dev/sdb,/dev/sdc)
 
 function check() {
   # 检测硬盘温度
@@ -13,7 +13,7 @@ function check() {
   if [ $temp -gt 50 ]; then
     echo "$1 当前硬盘温度是 $temp 大于设置的温度50 现在执行关机."
     #    shutdown -h now
-#    poweroff
+    #    poweroff
 
   else
     echo "$1 当前硬盘温度是 $temp"
