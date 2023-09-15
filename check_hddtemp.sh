51f8caba20da96805312eafa9af92f48449e64b3
xiaokai <xiaokai@kk.com>
2023-09-15 15:50:17 +0700

    打开sshtemp 温度超过50 关机

--- check_hddtemp.sh
+++ check_hddtemp.sh
@@ -15,7 +15,7 @@ function check() {
   if [ $temp -gt 50 ]; then
     echo "$1 当前硬盘温度是 $temp 大于设置的温度50 现在执行关机."
     #    shutdown -h now
-    #    poweroff
+    poweroff
 
   else
     echo "$1 当前硬盘温度是 $temp"
