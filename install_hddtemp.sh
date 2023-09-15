83e522c5c72eecf89ce1f42f464bafde2ad504d3
xiaokai <xiaokai@kk.com>
2023-09-15 16:14:20 +0700

    增加 r选项 从新安装 刷新sh文件

--- install_hddtemp.sh
+++ install_hddtemp.sh
@@ -3,7 +3,7 @@
 function echo_green() {
   echo -e "\033[32m$1\033[0m"
 }
-echo_green "是否执行安装自动检测硬盘温度 如果超过预设值则关机 y/n"
+echo_green "是否执行安装自动检测硬盘温度 如果超过预设值则关机 y/r/n"
 read answer
 if [ "$answer" == "y" ]; then
   cd $(dirname $0)
@@ -19,5 +19,16 @@ if [ "$answer" == "y" ]; then
     sudo crontab -l >conf && echo "*/5 * * * * check_hddtemp.sh" >>conf && sudo crontab conf && sudo rm -f conf
   fi
   echo "ok已经添加定时执行 自动检测硬盘温度 如果超过预设值则关机"
-
+elif [ "$answer" == "r" ]; then
+  cd $(dirname $0)
+  if [ $EUID -eq 0 ]; then
+    echo "当前用户是root用户"
+    cp ./check_hddtemp.sh /bin/check_hddtemp.sh
+    chmod 777 /bin/check_hddtemp.sh
+  else
+    echo "当前用户不是root用户"
+    sudo cp ./check_hddtemp.sh /bin/check_hddtemp.sh
+    sudo chmod 777 /bin/check_hddtemp.sh
+  fi
+  echo "ok已经覆盖了 /bin/check_hddtemp.sh 文件"
 fi
--- install_ping.sh
+++ install_ping.sh
@@ -4,7 +4,7 @@
 function echo_green() {
   echo -e "\033[32m$1\033[0m"
 }
-echo_green "是否执行ping不通自动关机的脚本添加到系统定时执行?(y/n)"
+echo_green "是否执行ping不通自动关机的脚本添加到系统定时执行?(y/r/n)"
 read answer
 if [ "$answer" == "y" ]; then
   cd $(dirname $0)
@@ -27,5 +27,19 @@ if [ "$answer" == "y" ]; then
 
   #  sudo rm -rf ./install_ping.sh
   echo "ok已经添加定时执行检测路由器是否在线"
+elif [ "$answer" == "r" ]; then
+  cd $(dirname $0)
+  echo_green "请输入需要ping的ip地址 一般填写路由器的ip即可"
+  read input_ip
 
+  sed "s/0.0.0.0/$input_ip/g" -i ./auto_ping.sh
+  if [ $EUID -eq 0 ]; then
+    echo "当前用户是root用户"
+    cp ./auto_ping.sh /bin/yy_auto_ping.sh
+    chmod 777 /bin/yy_auto_ping.sh
+  else
+    echo "当前用户不是root用户"
+    sudo cp ./auto_ping.sh /bin/yy_auto_ping.sh
+    sudo chmod 777 /bin/yy_auto_ping.sh
+  fi
 fi
