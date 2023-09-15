57157582f318e9c070dde26b532905a942a9a274
xiaokai <xiaokai@kk.com>
2023-09-15 15:21:41 +0700

    hddtemp开启关机

--- check_hddtemp.sh
+++ check_hddtemp.sh
@@ -7,11 +7,11 @@ temp=$(hddtemp /dev/sdc)
 temp=${temp:31:2}
 echo "$temp"
 ## 值是否大于50度
-#if [ $temp -gt 50 ]; then
-#  echo "当前硬盘温度是 $temp 大于设置的温度50 现在执行关机."
-#  #    shutdown -h now
-#  poweroff
-#
-#else
-#  echo "当前硬盘温度是 $temp"
-#fi
+if [ $temp -gt 50 ]; then
+  echo "当前硬盘温度是 $temp 大于设置的温度50 现在执行关机."
+  #    shutdown -h now
+  poweroff
+
+else
+  echo "当前硬盘温度是 $temp"
+fi
