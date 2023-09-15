5c862a24f21d0c9305462514e5141636b259e354
xiaokai <xiaokai@kk.com>
2023-09-15 15:31:12 +0700

    数组测试hdd

--- check_hddtemp.sh
+++ check_hddtemp.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-# 检测硬盘温度
-temp=$(hddtemp /dev/sdc)
+hdds=("/dev/sdb","/dev/sdc")
 
-# 货物温度值
-temp=${temp:31:2}
-echo "$temp"
-## 值是否大于50度
-if [ $temp -gt 50 ]; then
-  echo "当前硬盘温度是 $temp 大于设置的温度50 现在执行关机."
-  #    shutdown -h now
-  poweroff
+function check() {
+  # 检测硬盘温度
+  temp=$(hddtemp $1)
 
-else
-  echo "当前硬盘温度是 $temp"
-fi
+  # 货物温度值
+  temp=${temp:31:2}
+  echo "$temp"
+  ## 值是否大于50度
+  if [ $temp -gt 50 ]; then
+    echo "$1 当前硬盘温度是 $temp 大于设置的温度50 现在执行关机."
+    #    shutdown -h now
+#    poweroff
+
+  else
+    echo "$1 当前硬盘温度是 $temp"
+  fi
+
+}
+
+# 遍历数组中的元素和下标
+for i in "${!hdds[@]}"; do
+  # 在此处执行操作，使用 $i 和 ${array[$i]} 处理每个元素和下标
+  check $i
+done
