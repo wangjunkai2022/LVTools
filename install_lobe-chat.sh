229d80fb0965e6f163a0cb1b639c3238987b85c7
xiaokai <xiaokai@kk.com>
2024-02-05 21:27:08 +0700

    lobe-chat

--- install_lobe-chat.sh
+++ install_lobe-chat.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd $(dirname $0)
-#sudo tar -xzvf lobe-chat.tgz -C /opt
+curl -O -L https://github.com/wangjunkai2022/lobe-chat/releases/download/1.0/lobe-chat.tgz
+sudo tar -xzvf lobe-chat.tgz -C /opt
 # 解压gz分卷
-cd lobe-chat
-sudo cat lobe-chat.tar.gz* | tar zx
-sudo mv lobe-chat /opt
+#cd lobe-chat
+#sudo cat lobe-chat.tar.gz* | tar zx
+#sudo mv lobe-chat /opt
 cp /opt/lobe-chat/lobe-chat.service /etc/systemd/system/
 sudo systemctl enable lobe-chat.service
 sudo systemctl start lobe-chat.service
