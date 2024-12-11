f674239639e2fd62bc1e4ff1c1a3dfd2c13d584e
xiaokai <xiaokai@kk.com>
2024-12-11 19:59:19 +0700

    alist 入口修改

--- docker_compose/video/alist_entrypoint.sh
+++ docker_compose/video/alist_entrypoint.sh
@@ -5,20 +5,18 @@ chown -R ${PUID}:${PGID} /opt/alist/
 umask ${UMASK}
 
 echo alist启动入口修改版
-echo alist 服务启动完毕
-    # 检查是否为第一次启动，通过查看一个特定文件是否存在
-    if [ ! -f /opt/alist/.initialized ]; then
-        # 检查 ALIST_PASSWORD 环境变量是否存在
-        if [ -n "${ALIST_PASSWORD}" ]; then
-            echo "第一次运行 这里等待60秒会重新设置一下密码为:${ALIST_PASSWORD}"
-            sleep 60
-            ./alist admin set "${ALIST_PASSWORD}"  # 设置密码
-        else
-            echo "不设置默认密码"
-        fi
-        touch /opt/alist/.initialized  # 创建标记文件，表示已初始化
+# 检查是否为第一次启动，通过查看一个特定文件是否存在
+if [ ! -f /opt/alist/.initialized ]; then
+    # 检查 ALIST_PASSWORD 环境变量是否存在
+    if [ -n "${ALIST_PASSWORD}" ]; then
+        echo "第一次运行 这里重新设置一下密码为:${ALIST_PASSWORD}"
+        ./alist admin set "${ALIST_PASSWORD}"  # 设置密码
+    else
+        echo "不设置默认密码"
     fi
-    
+    touch /opt/alist/.initialized  # 创建标记文件，表示已初始化
+fi
+
 if [ "$1" = "version" ]; then
     ./alist version
 else
